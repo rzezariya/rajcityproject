c164e59f8a9f7fc1f6a725945e555f37b3b95159
= <rzezari16@gmail.com>
2023-05-31 08:42:43 +0530

    first commit

---
+++ database/migrations/2023_05_31_030030_create_currency_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currency', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',10)->index();
+            $table->string('symbol',25);
+            $table->string('format',50);
+            $table->string('exchange_rate');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currency');
+    }
+};
